1b1a054688f4dad2942c0709639ae3646db77cf9
rhgroupconnector <rhgroup.connector@outlook.com>
2020-06-01 19:58:25 +0800

    Image update

--- src/screens/device.js
+++ src/screens/device.js
@@ -8,8 +8,11 @@ class DeviceView extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            imgSrc : ""
+            imgSrc : "",
+            scrollPost: "",
+            innerWidth : ""
         }
+        this.useScrollPosition = this.useScrollPosition.bind(this);
     }
     routeTo = (path, state) => {
         this.props.history.push(path, [state])
@@ -18,12 +21,32 @@ class DeviceView extends React.Component{
     async componentDidMount(){
         let imgSrc = await this.props.location.state[0].imgSrc[0].url;
         this.setState({imgSrc})
+        window.addEventListener('scroll', this.useScrollPosition, true)
+        window.addEventListener('resize', this.resizeWindow, true)
+    }
+
+    resizeWindow(event){
+        // let windowWidth = window.innerWidth;
+
+        // if(windowWidth > 480){
+                
+        // }else{
+
+        // }
+    }
+
+    useScrollPosition(event){
+        let scrollTop = event.srcElement.scrollingElement.scrollTop,
+             itemTranslate = Math.min(0, scrollTop/3 - 60);
+        this.setState({
+            scrollPos: itemTranslate
+        })
     }
     render(){
         return(
-            <Container>
+            <Container fluid className="fullscreen">
                 <Columns>
-                    <Columns.Column>
+                    <Columns.Column size={12} className="sticky" >
                         <Button color="primary" onClick={() => this.routeTo("/")}>
                             <FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon>
                             <span>Go back</span>
