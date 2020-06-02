9d1bf300d9b061bba025d42dc8fa0863c77c4b7b
rhgroupconnector <rhgroup.connector@outlook.com>
2020-06-02 10:40:37 +0800

    Achieve screen detection functionality

--- src/screens/device.js
+++ src/screens/device.js
@@ -12,48 +12,74 @@ class DeviceView extends React.Component{
             scrollPost: "",
             innerWidth : ""
         }
+        this._isMounted = false;
         this.useScrollPosition = this.useScrollPosition.bind(this);
+        this.resizeWindow = this.resizeWindow.bind(this);
+        this.propsState = this.props.location.state[0];
     }
     routeTo = (path, state) => {
         this.props.history.push(path, [state])
     }
 
-    async componentDidMount(){
-        let imgSrc = await this.props.location.state[0].imgSrc[0].url;
-        this.setState({imgSrc})
-        window.addEventListener('scroll', this.useScrollPosition, true)
-        window.addEventListener('resize', this.resizeWindow, true)
+    componentDidMount(){
+        this._isMounted = true;
+
+        if(this._isMounted){
+            let imgSrc= this.propsState.imgSrcDefault;
+            this.setState({imgSrc});
+
+            window.addEventListener('scroll', this.useScrollPosition)
+            window.addEventListener('resize', this.resizeWindow)
+        }
     }
 
-    resizeWindow(event){
-        // let windowWidth = window.innerWidth;
+    componentWillUnmount(){
+        this._isMounted = false;
+        window.removeEventListener('scroll', this.useScrollPosition)
+        window.removeEventListener('resize', this.resizeWindow)
+    }
 
-        // if(windowWidth > 480){
-                
-        // }else{
+    resizeWindow(){
+        let windowWidth = window.innerWidth;
+        let currentUrl = this.props.match.url;
+        let pathName = "../" + this.props.match.params.slug + "/";
 
-        // }
+        if(currentUrl.includes("desktop") && windowWidth <= 480 && this._isMounted){
+            this.routeTo(pathName + "mobile");
+            this.setState({
+                imgSrc: this.propsState.imgSrcSub
+            });
+        }
+      
+
+        if(currentUrl.includes("mobile") && windowWidth > 480 && this._isMounted){
+                this.routeTo(pathName + "desktop");
+                this.setState({
+                    imgSrc: this.propsState.imgSrcDefault
+                });
+        }
     }
 
     useScrollPosition(event){
-        let scrollTop = event.srcElement.scrollingElement.scrollTop,
-             itemTranslate = Math.min(0, scrollTop/3 - 60);
+        let scrollTop = event.srcElement.scrollingElement.scrollTop;
+
         this.setState({
-            scrollPos: itemTranslate
+            scrollPos: scrollTop
         })
     }
+
     render(){
         return(
             <Container fluid className="fullscreen">
                 <Columns>
-                    <Columns.Column size={12} className="sticky" >
+                    <Columns.Column size={12} className={this.state.scrollPos > 40 ? "sticky sticky-full" : "sticky"}>
                         <Button color="primary" onClick={() => this.routeTo("/")}>
                             <FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon>
                             <span>Go back</span>
                         </Button>
                     </Columns.Column>
                     <Columns.Column size={12}>
-                        { this.state.imgSrc == "" 
+                        { this.state.imgSrc === "" 
                         ?   <p>Loading data ...</p>
                         :   <Image src={this.state.imgSrc} />
                         }
